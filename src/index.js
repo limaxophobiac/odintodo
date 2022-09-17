b975cee241ec743411fb58bf7a99e908cdca62cf
limax <joel_bolkert@hotmail.com>
2022-09-17 03:33:00 +0200

    s

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import './style.css';
 let activeProject = null;
 let defaultProject = projectFactory("NONE", "default project");
 addProject(defaultProject);
-refreshProjects();
 
 const doContainer = document.getElementById("taskcontainer");
 const projectContainer = document.getElementById("projectList");
